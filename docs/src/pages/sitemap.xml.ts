0bc3fb76eb5a1c791b18bf10ba0b8535c38bf452
Max <max.lindqvist@ipp.mpg.de>
2026-09-04 08:49:32 +0200

    Bugfix

--- docs/src/pages/sitemap.xml.ts
+++ docs/src/pages/sitemap.xml.ts
@@ -1,16 +1,13 @@
 import rawModels from '../data/models.json';
+import catalogueIndex from '../data/catalogue-index.json';
 import { toSlug } from '../lib/catalogue';
 
 export async function GET() {
-  const equilibriumModules = import.meta.glob('../data/equilibria/*.json', { eager: true, import: 'default' });
-  const perturbationModules = import.meta.glob('../data/perturbations/*.json', { eager: true, import: 'default' });
-  const equilibria = [...new Set(Object.values(equilibriumModules).map((item: any) => item.type))];
-  const perturbations = [...new Set(Object.values(perturbationModules).map((item: any) => item.type))];
   const paths = [
     '/', '/models/', '/domains/', '/equilibria/', '/perturbations/', '/search/', '/citation/',
     ...rawModels.map((model: any) => `/models/${toSlug(model.className)}/`),
-    ...equilibria.map((name) => `/equilibria/${toSlug(name)}/`),
-    ...perturbations.map((name) => `/perturbations/${toSlug(name)}/`),
+    ...catalogueIndex.equilibria.map((item) => `/equilibria/${item.slug}/`),
+    ...catalogueIndex.perturbations.map((item) => `/perturbations/${item.slug}/`),
   ];
   const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${paths.map((path) => `  <url><loc>https://struphy-hub.github.io${path}</loc></url>`).join('\n')}\n</urlset>\n`;
   return new Response(body, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } });
