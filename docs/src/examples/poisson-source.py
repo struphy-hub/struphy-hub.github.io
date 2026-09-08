1252e12161846baeddeedc7e92346369bd2677d1
Max <max.lindqvist@ipp.mpg.de>
2026-09-08 18:48:42 +0300

    new runs

--- docs/src/examples/poisson-source.py
+++ docs/src/examples/poisson-source.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
                     {
                         "label": "Play",
                         "method": "animate",
-                        "args": [None, {"frame": {"duration": 120, "redraw": True}, "fromcurrent": True}],
+                        "args": [None, {"frame": {"duration": 30, "redraw": True}, "fromcurrent": True}],
                     },
                 ],
             },
